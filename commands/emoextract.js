7cb6bafb17b66c2a94d44bfa58121c2023a3ed53
TANDJINKWINGARIDELRONIS <tandjiridel@gmail.com>
2026-02-09 05:55:03 +0100

    commit

--- commands/emoextract.js
+++ commands/emoextract.js
@@ -1,6 +1,6 @@
 const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
 
-async function extractCommand(sock, chatId, message) {
+async function extractCommand(sock,chatId,message) {
     // Extract quoted imageMessage or videoMessage from your structure
     const quoted = message.message?.extendedTextMessage?.contextInfo?.quotedMessage;
     const quotedImage = quoted?.imageMessage;
@@ -11,15 +11,15 @@ async function extractCommand(sock, chatId, message) {
         const stream = await downloadContentFromMessage(quotedImage, 'image');
         let buffer = Buffer.from([]);
         for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk]);
-        await sock.sendMessage(ownerJid, { image: buffer, fileName: 'media.jpg', caption: quotedImage.caption || '' }, { quoted: message });
+        await sock.sendMessage(chatId, { image: buffer, fileName: 'media.jpg', caption: quotedImage.caption || '' }, { quoted: message });
     } else if (quotedVideo && quotedVideo.viewOnce) {
         // Download and send the video
         const stream = await downloadContentFromMessage(quotedVideo, 'video');
         let buffer = Buffer.from([]);
         for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk]);
-        await sock.sendMessage(ownerJid, { video: buffer, fileName: 'media.mp4', caption: quotedVideo.caption || '' }, { quoted: message });
+        await sock.sendMessage(chatId, { video: buffer, fileName: 'media.mp4', caption: quotedVideo.caption || '' }, { quoted: message });
     } else {
-        await sock.sendMessage(ownerJid, { text: '❌ Re-essayer en repondant a l\'image ou video.' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '❌ Re-essayer en repondant a l\'image ou video.' }, { quoted: message });
     }
 }
 
